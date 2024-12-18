ec6d525ccd77e904b210d73478bf9cea898fe4cf
Dijkstra <chris.dijkstra@wur.nl>
2024-12-18 12:17:04 +0100

    Added clause for all variants missing, will output maf as NA

--- calc_hf.py
+++ calc_hf.py
@@ -55,10 +55,11 @@ def calc_het_corr_freqs(line: list[str | int]) -> dict[str, str | int]:
     missing_f = missing / (hom1 + hom2 + het + missing)
     het_adjusted_missing_f = (het + missing) / (hom1 + hom2 + het + missing)
     minor_hom = min(hom1, hom2)
-    maf = (minor_hom * 2 + het) / ((hom1 + hom2 + het) * 2)
 
-    # If only het and missing, set corrected maf to 0
-    het_adjusted_maf = 0 if het_adjusted_missing_f == 1 else minor_hom * 2 / ((hom1 + hom2) * 2)
+    # If only missing, set maf to NA
+    maf = "NA" if missing_f == 1 else (minor_hom * 2 + het) / ((hom1 + hom2 + het) * 2)
+    # If only het and missing, set corrected maf to NA
+    het_adjusted_maf = "NA" if het_adjusted_missing_f == 1 else minor_hom * 2 / ((hom1 + hom2) * 2)
     return {"ID": var_id, "CORR_MAF": het_adjusted_maf, "MAF": maf,
             "CORR_MISS_FRQ": het_adjusted_missing_f, "MISS_FRQ": missing_f}
 
