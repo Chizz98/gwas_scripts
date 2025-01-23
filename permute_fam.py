4dc456d2b1f55e77967d13bf1f4087422bd387cb
Dijkstra <chris.dijkstra@wur.nl>
2025-01-23 11:33:10 +0100

    Added cmd functionality

--- permute_fam.py
+++ permute_fam.py
@@ -1,22 +1,57 @@
 import pandas as pd
 import numpy as np
+import argparse as arg
+
+
+def arg_reader():
+    """ Reads arguments from command line
+
+    :return class containing the arguments
+    """
+    arg_parser = arg.ArgumentParser(
+        description="Takes a .fam file and permutes the specified phenotype, keeping NAs"
+                    "in the same position"
+    )
+    arg_parser.add_argument(
+        "infile",
+        help="A .fam file"
+    )
+    arg_parser.add_argument(
+        "phenotype",
+        help="The phenotype to permute, with 1 being the first phenotype column",
+        type=int
+    )
+    arg_parser.add_argument(
+        "permute_n",
+        help="The number of permutations to make",
+        type=int
+    )
+    arg_parser.add_argument(
+        "out_name",
+        help="The prefix of the outfile"
+    )
+    return arg_parser.parse_args()
 
 
 def main():
-    pheno_of_interest = 6
-    fam_file = "LKser_bed_files_v4.fam"
+    args = arg_reader()
 
-    fam_file = pd.read_table(fam_file, header=None)
+    pheno_of_interest = 4 + args.phenotype
+    fam_file = args.infile
+    n_permute = args.permute_n
+    out_name = args.out_name
+
+    fam_file = pd.read_table(fam_file, header=None, delim_whitespace=True)
     fam_base = fam_file.iloc[:, 0:5]
     phenotype_col = fam_file.iloc[:, pheno_of_interest].copy()
     non_na = phenotype_col.dropna().tolist()
 
-    for i in range(10):
+    for i in range(n_permute):
         np.random.shuffle(non_na)
         phenotype_col.loc[phenotype_col.notna()] = non_na
         fam_base = pd.concat([fam_base, phenotype_col], axis=1)
 
-    fam_base.to_csv("out.fam", sep="\t", header=False, index=False, na_rep="NA")
+    fam_base.to_csv(out_name + ".fam", sep="\t", header=False, index=False, na_rep="NA")
 
 
 if __name__ == "__main__":
